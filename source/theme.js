41f95fe407db51081ce82a0fd6c78853f55c76ff
Trevin Chow <trevin@trevinchow.com>
2025-03-14 22:52:42 -0700

    feat: bnpl messaging on product and cart pages

--- source/theme.js
+++ source/theme.js
@@ -1,5 +1,6 @@
 //= require_directory ./javascripts/vendor
 //= require javascripts/functions
+//= require javascripts/product-payment-messaging
 //= require javascripts/product
 //= require javascripts/store
 //= require javascripts/navigation
